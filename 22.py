1972392975f4de13015a07febfdc9659de2d9b57
전이현 <kid_a_@naver.com>
2019-03-03 11:43:16 +0900

    Python study week 3

---
+++ 22.py
@@ -0,0 +1,19 @@
+a=True
+b=False
+print(a==1)
+print(b!=0)
+
+x=1;y=2
+str1='abc';str2='python'
+print(x==y)
+print(x!=y)
+print(str1==str2)
+print(str2=='python')
+print(str1<str2)
+boo11=True;boo12=False;boo13=True;boo14=False
+print(boo11 and boo12)
+print(boo11 and boo13)
+print(boo12 or boo13)
+print(boo12 or boo14)
+print(not boo11)
+print(not boo12)
---
+++ test 5-10.py
@@ -0,0 +1,53 @@
+# 주석 처리 예시임
+# 만든 날짜: 2019. 03. 01
+a = 1
+b = 5
+print(a+b)
+
+"""a=1
+b=5
+print(a+b)"""
+a=2
+b=6
+print(a+b)
+
+int_data = 1
+float_date = 3.14
+complex_data = 1 + 5j
+str_data1 = 'I love Python'
+str_data1 = "반갑습니다."
+list_data = [1,2,3]
+tuple_data = (1,2,3)
+dict_data = {0:'False', 1:'true'}
+
+a=200
+msg='I love Python'
+list_data=['a','b','c']
+dict_data={'a':97,'b':98}
+print(a)
+print(msg)
+print(list_data)
+print(list_data[0])
+print(dict_data)
+print(dict_data['a'])
+
+listdata=['a','b','c']
+if 'a' in listdata:
+        print('a가 listdata에 있습니다.')
+        print(listdata)
+else:
+    print('a가 listdata에 존재하지 않습니다.')
+
+x=1
+y=2
+if x>=y:
+    print('x가 y보다 작습니다.')
+else:
+    print('x가 y보다 작습니다.')
+
+x=1
+y=2
+if x>y:
+    print('x가 y보다 큽니다.')
+else:
+    print('x가 y가 같습니다.')
